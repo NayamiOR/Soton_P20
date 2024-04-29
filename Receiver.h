a62ec7451214b0c96c4cc291bc32175d3d4274d3
安逐悲 <nayamior@outlook.com>
2024-04-29 16:08:53 +0100

    create sender and receiver class

---
+++ Receiver.h
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2024/4/29.
+//
+
+#ifndef P20_CODE_RECEIVER_H
+#define P20_CODE_RECEIVER_H
+
+
+class Receiver {
+
+};
+
+
+#endif //P20_CODE_RECEIVER_H
---
+++ Sender.h
@@ -0,0 +1,14 @@
+//
+// Created by Administrator on 2024/4/29.
+//
+
+#ifndef P20_CODE_SENDER_H
+#define P20_CODE_SENDER_H
+
+
+class Sender {
+
+};
+
+
+#endif //P20_CODE_SENDER_H
